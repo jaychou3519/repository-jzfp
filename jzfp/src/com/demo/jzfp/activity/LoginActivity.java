098fd4cec0b9921154ba6e02b1641b71e28fe06e
Administrator <Administrator@PC-20150901UZMH>
2016-09-06 18:00:10 +0800

    cs

--- jzfp/src/com/demo/jzfp/activity/LoginActivity.java
+++ jzfp/src/com/demo/jzfp/activity/LoginActivity.java
@@ -37,7 +37,6 @@ public class LoginActivity extends BaseActivity implements OnClickListener{
 
 	@Override
 	protected void initData() {
-		System.out.println("aaaa");
 		sp = getSharedPreferences("user", Context.MODE_PRIVATE);
 		et_username.setText(sp.getString("username", ""));
 		et_password.setText(sp.getString("password", ""));
