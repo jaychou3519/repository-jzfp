2b5503db431492faea837b084fc6c368e05de945
Administrator <Administrator@PC-20151113BSSS>
2016-09-07 10:37:44 +0800

    res

---
+++ jzfp/src/com/demo/jzfp/utils/Constant.java
@@ -0,0 +1,19 @@
+package com.demo.jzfp.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Constant {
+	
+	/**
+	 * 基础url
+	 * */
+	public static String baseUrl = "http://mmj.zksr.cn/gold/app/salesman_app!";
+	public static String baseUrl_use = "http://mmj.zksr.cn/gold/app/app!";
+	public static String imageUrl="http://mmj.zksr.cn/gold";
+	
+
+
+	public static String weather="http://api.map.baidu.com/telematics/v3/weather?location=炎陵县&output=json&ak=NqKsiqWINA3Mvknew2WXMXhp9GgI0bp4";
+}
