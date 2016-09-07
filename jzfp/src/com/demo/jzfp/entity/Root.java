33a6b79430fc09ae78ee18855924d46a6e7cf029
Administrator <Administrator@PC-20151113BSSS>
2016-09-07 15:08:50 +0800

    drawble

---
+++ jzfp/src/com/demo/jzfp/entity/Root.java
@@ -0,0 +1,45 @@
+package com.demo.jzfp.entity;
+
+import java.util.List;
+
+public class Root {
+	private int error;
+
+	private String status;
+
+	private String date;
+
+	private List<Results> results;
+
+	public void setError(int error) {
+		this.error = error;
+	}
+
+	public int getError() {
+		return this.error;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDate() {
+		return this.date;
+	}
+
+	public void setResults(List<Results> results) {
+		this.results = results;
+	}
+
+	public List<Results> getResults() {
+		return this.results;
+	}
+}
