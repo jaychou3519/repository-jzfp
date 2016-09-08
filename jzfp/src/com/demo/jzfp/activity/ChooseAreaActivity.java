f184b0a08ddabe3b39763d9be782b199bbc41acf
jaychou3519 <45279200@qq.com>
2016-09-08 15:26:31 +0800

    aa

--- jzfp/src/com/demo/jzfp/activity/ChooseAreaActivity.java
+++ jzfp/src/com/demo/jzfp/activity/ChooseAreaActivity.java
@@ -1,8 +1,20 @@
 package com.demo.jzfp.activity;
-
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+import com.alibaba.fastjson.JSON;
 import com.demo.jzfp.R;
+import com.demo.jzfp.utils.WebServiceClient;
 
 public class ChooseAreaActivity extends BaseActivity{
+	
+	private static String result;
+    private static String METHOD_NAME = "selectToRegister";
+	
 
 	@Override
 	protected void setView() {
@@ -17,6 +29,52 @@ public class ChooseAreaActivity extends BaseActivity{
 	@Override
 	protected void initData() {
 		
+	}
+	
+	MyHandler myHandler = new MyHandler(this);
+	
+	public void callWeb(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                result = WebServiceClient.callWeb(METHOD_NAME,null);
+                myHandler.sendEmptyMessage(1);
+            }
+        }).start();
+    }
+	
+	static class MyHandler extends Handler{
+	        WeakReference<ChooseAreaActivity> mActivity;
+	
+	        MyHandler(ChooseAreaActivity theActivity) {
+	            mActivity = new WeakReference<ChooseAreaActivity>(theActivity);
+	        }
+	
+	        @Override
+	        public void handleMessage(Message msg) {
+	        	ChooseAreaActivity myActivity = mActivity.get();
+	            switch (msg.what) {
+	                case 1:
+	                	generateViewList(myActivity);
+	                    break;
+	            }
+	            super.handleMessage(msg);
+	        }
+	        
+	        public void generateViewList(ChooseAreaActivity context){
+	    		List<Map> map = JSON.parseArray(result, Map.class);
+	    		TextView area = (TextView)context.findViewById(R.id.area);
+	    		List<Map> towns = new ArrayList<Map>();
+	    		List<Map> villages = new ArrayList<Map>();
+	    		for (Map map2 : map) {
+	    			String name = (String) map2.get("name");
+	    			if(name.contains("县") || name.contains("镇") || name.contains("乡")){
+	    				towns.add(map2);
+	    			}else{
+	    				villages.add(map2);
+	    			}
+				}
+	        }
 	}
 
 }
--- jzfp/src/com/demo/jzfp/utils/WebServiceClient.java
+++ jzfp/src/com/demo/jzfp/utils/WebServiceClient.java
@@ -1,8 +1,16 @@
 package com.demo.jzfp.utils;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
@@ -11,44 +19,57 @@ import org.ksoap2.transport.HttpTransportSE;
 import android.util.Log;
 
 public class WebServiceClient {
-    
-    public static String callWeb(String nameSpace, String methodName,String serverURL, 
-    		String interfaceName, Map<String,Object> params){
-        //每一次网络请求都会重新调用
-        String resultString = null;
-        // 实例化SoapObject对象
-        SoapObject request = new SoapObject(nameSpace, methodName);
-        /**
-         * 设置参数，参数名不一定需要跟调用的服务器端的参数名相同，只需要对应的顺序相同即可
-         * */
-        if(null != params){
-	         Set<String> keySet = params.keySet();
-	         for(String key:keySet){
-	        	 request.addProperty(key, params.get(key));
-	         }
-        }
-        // 使用soap1.1协议创建Envelop对象
-        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-        HttpTransportSE ht = null;
-        ht = new HttpTransportSE(serverURL + interfaceName, 10*1000);
-        ht.debug = true;
-        try {
-            // 将SoapObject对象设置为SoapSerializationEnvelope对象的传出SOAP消息
-            envelope.bodyOut = request;
-            // 调用webService
-            ht.call(null, envelope);
-            if (envelope.getResponse() != null) {
-                SoapObject result = (SoapObject) envelope.bodyIn;
-                resultString = result.getProperty(0).toString();
-                Log.i("调用返回信息", resultString+"");
-            }else {
-                Log.i("调用返回信息", "调用失败，返回数据为null");
-            }
-        }catch(OutOfMemoryError e){
-            e.printStackTrace();
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        return resultString;
-    }
+	
+	public static String NAME_SPACE = "http://webservice.item.hihsoft.com/";
+	public static String SERVER_URL = "http://115.28.14.1:9145/ylppbd/services/ylppbdWS/";
+	
+	/**
+	 * 调用WebService
+	 * 
+	 * @param nameSpace
+	 * @param methodName
+	 * @param serverURL
+	 * @param interfaceName
+	 * @param params
+	 * @return
+	 */
+	public static String callWeb(String methodName,Map<String, Object> params) {
+		// 每一次网络请求都会重新调用
+		String resultString = null;
+		// 实例化SoapObject对象
+		SoapObject request = new SoapObject(NAME_SPACE, methodName);
+		/**
+		 * 设置参数，参数名不一定需要跟调用的服务器端的参数名相同，只需要对应的顺序相同即可
+		 * */
+		if (null != params) {
+			Set<String> keySet = params.keySet();
+			for (String key : keySet) {
+				request.addProperty(key, params.get(key));
+			}
+		}
+		// 使用soap1.1协议创建Envelop对象
+		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+				SoapEnvelope.VER11);
+		HttpTransportSE ht = null;
+		ht = new HttpTransportSE(SERVER_URL + methodName, 10 * 1000);
+		ht.debug = true;
+		try {
+			// 将SoapObject对象设置为SoapSerializationEnvelope对象的传出SOAP消息
+			envelope.bodyOut = request;
+			// 调用webService
+			ht.call(null, envelope);
+			if (envelope.getResponse() != null) {
+				SoapObject result = (SoapObject) envelope.bodyIn;
+				resultString = result.getProperty(0).toString();
+				Log.i("调用返回信息:",resultString);
+			} else {
+				Log.i("调用返回信息", "调用失败，返回数据为null");
+			}
+		} catch (OutOfMemoryError e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultString;
+	}
 }
