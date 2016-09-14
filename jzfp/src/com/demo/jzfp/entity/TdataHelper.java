5701740b1abd99730e8fc967abc22d7d9337c284
jaychou3519 <45279200@qq.com>
2016-09-14 09:27:31 +0800

    aa

--- jzfp/src/com/demo/jzfp/entity/TdataHelper.java
+++ jzfp/src/com/demo/jzfp/entity/TdataHelper.java
@@ -7,6 +7,38 @@ public class TdataHelper {
 	private String jdbfzrOrger;//负责人
 	private String bfzrrPhone;//负责人电话
 	private String jdbfzrr;//帮扶负责人
+	/**
+     * 帮扶户所在地乡（镇）党委书记姓名       db_column: DWSJ_NAME 
+     */	
+	private java.lang.String dwsjName;
+    /**
+     * 帮扶户所在地乡（镇）党委书记联系电话       db_column: dwsj_phone 
+     */	
+	private java.lang.String dwsjPhone;
+    /**
+     * 帮扶户所在地乡（镇）长姓名       db_column: zz_name 
+     */	
+	private java.lang.String zzName;
+    /**
+     * 帮扶户所在地乡（镇）长联系电话       db_column: zz_phone 
+     */	
+	private java.lang.String zzPhone;
+    /**
+     * 帮扶户所在乡（镇）村 书记姓名       db_column: csj_name 
+     */	
+	private java.lang.String csjName;
+    /**
+     * 帮扶户所在乡（镇）村 书记联系电话       db_column: cxj_phone 
+     */	
+	private java.lang.String cxjPhone;
+    /**
+     * 帮扶户所在乡（镇）村主任姓名       db_column: czr_name 
+     */	
+	private java.lang.String czrName;
+    /**
+     * 帮扶户所在乡（镇）村主任联系电话       db_column: czr_phone 
+     */	
+	private java.lang.String czrPhone;
 	public String getJdbfzrOrgname() {
 		return jdbfzrOrgname;
 	}
@@ -30,6 +62,54 @@ public class TdataHelper {
 	}
 	public void setJdbfzrr(String jdbfzrr) {
 		this.jdbfzrr = jdbfzrr;
+	}
+	public java.lang.String getDwsjName() {
+		return dwsjName;
+	}
+	public void setDwsjName(java.lang.String dwsjName) {
+		this.dwsjName = dwsjName;
+	}
+	public java.lang.String getDwsjPhone() {
+		return dwsjPhone;
+	}
+	public void setDwsjPhone(java.lang.String dwsjPhone) {
+		this.dwsjPhone = dwsjPhone;
+	}
+	public java.lang.String getZzName() {
+		return zzName;
+	}
+	public void setZzName(java.lang.String zzName) {
+		this.zzName = zzName;
+	}
+	public java.lang.String getZzPhone() {
+		return zzPhone;
+	}
+	public void setZzPhone(java.lang.String zzPhone) {
+		this.zzPhone = zzPhone;
+	}
+	public java.lang.String getCsjName() {
+		return csjName;
+	}
+	public void setCsjName(java.lang.String csjName) {
+		this.csjName = csjName;
+	}
+	public java.lang.String getCxjPhone() {
+		return cxjPhone;
+	}
+	public void setCxjPhone(java.lang.String cxjPhone) {
+		this.cxjPhone = cxjPhone;
+	}
+	public java.lang.String getCzrName() {
+		return czrName;
+	}
+	public void setCzrName(java.lang.String czrName) {
+		this.czrName = czrName;
+	}
+	public java.lang.String getCzrPhone() {
+		return czrPhone;
+	}
+	public void setCzrPhone(java.lang.String czrPhone) {
+		this.czrPhone = czrPhone;
 	}
 }
 
