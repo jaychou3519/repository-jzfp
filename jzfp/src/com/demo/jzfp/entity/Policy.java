1408e74e5bc56edae488a43e1d8ab2612fda2cd1
Administrator <Administrator@PC-20151113BSSS>
2016-09-14 15:36:09 +0800

    jjy

---
+++ jzfp/src/com/demo/jzfp/entity/Policy.java
@@ -0,0 +1,355 @@
+package com.demo.jzfp.entity;
+
+import java.util.List;
+
+public class Policy {
+	private String contextPath;
+
+	private String page;
+
+	private String sortname;
+
+	private String sortorder;
+
+	private String newsKey;
+
+	private String fw_url;
+
+	private String newsid;
+
+	private String categoryid;
+
+	private String newTitle;
+
+	private String userId;
+
+	private String deployTime;
+
+	private String addUserid;
+
+	private String addTime;
+
+	private String modifyUserid;
+
+	private String modifyTime;
+
+	private String content;
+
+	private String viewNum;
+
+	private String newsLogo;
+
+	private String status;
+
+	private String resource;
+
+	private String address;
+
+	private String newsSummlar;
+
+	private String itemid;
+
+	private String itemName;
+
+	private String url;
+
+	private String noone;
+
+	private String beginTime;
+
+	private String endTime;
+
+	private String categoryName;
+
+	private String userName;
+
+	private String addUserName;
+
+	private String modifyUserName;
+
+	private String displaying;
+
+	private List<TnewsAudits> tnewsAudits ;
+
+	private String aconfigs;
+	
+	public String getFw_url() {
+		return fw_url;
+	}
+
+	public void setFw_url(String fw_url) {
+		this.fw_url = fw_url;
+	}
+	
+	public void setContextPath(String contextPath) {
+		this.contextPath = contextPath;
+	}
+
+	public String getContextPath() {
+		return this.contextPath;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public String getPage() {
+		return this.page;
+	}
+
+	public void setSortname(String sortname) {
+		this.sortname = sortname;
+	}
+
+	public String getSortname() {
+		return this.sortname;
+	}
+
+	public void setSortorder(String sortorder) {
+		this.sortorder = sortorder;
+	}
+
+	public String getSortorder() {
+		return this.sortorder;
+	}
+
+	public void setNewsKey(String newsKey) {
+		this.newsKey = newsKey;
+	}
+
+	public String getNewsKey() {
+		return this.newsKey;
+	}
+
+	public void setNewsid(String newsid) {
+		this.newsid = newsid;
+	}
+
+	public String getNewsid() {
+		return this.newsid;
+	}
+
+	public void setCategoryid(String categoryid) {
+		this.categoryid = categoryid;
+	}
+
+	public String getCategoryid() {
+		return this.categoryid;
+	}
+
+	public void setNewTitle(String newTitle) {
+		this.newTitle = newTitle;
+	}
+
+	public String getNewTitle() {
+		return this.newTitle;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserId() {
+		return this.userId;
+	}
+
+	public void setDeployTime(String deployTime) {
+		this.deployTime = deployTime;
+	}
+
+	public String getDeployTime() {
+		return this.deployTime;
+	}
+
+	public void setAddUserid(String addUserid) {
+		this.addUserid = addUserid;
+	}
+
+	public String getAddUserid() {
+		return this.addUserid;
+	}
+
+	public void setAddTime(String addTime) {
+		this.addTime = addTime;
+	}
+
+	public String getAddTime() {
+		return this.addTime;
+	}
+
+	public void setModifyUserid(String modifyUserid) {
+		this.modifyUserid = modifyUserid;
+	}
+
+	public String getModifyUserid() {
+		return this.modifyUserid;
+	}
+
+	public void setModifyTime(String modifyTime) {
+		this.modifyTime = modifyTime;
+	}
+
+	public String getModifyTime() {
+		return this.modifyTime;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setViewNum(String viewNum) {
+		this.viewNum = viewNum;
+	}
+
+	public String getViewNum() {
+		return this.viewNum;
+	}
+
+	public void setNewsLogo(String newsLogo) {
+		this.newsLogo = newsLogo;
+	}
+
+	public String getNewsLogo() {
+		return this.newsLogo;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setResource(String resource) {
+		this.resource = resource;
+	}
+
+	public String getResource() {
+		return this.resource;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setNewsSummlar(String newsSummlar) {
+		this.newsSummlar = newsSummlar;
+	}
+
+	public String getNewsSummlar() {
+		return this.newsSummlar;
+	}
+
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
+
+	public String getItemid() {
+		return this.itemid;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemName() {
+		return this.itemName;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setNoone(String noone) {
+		this.noone = noone;
+	}
+
+	public String getNoone() {
+		return this.noone;
+	}
+
+	public void setBeginTime(String beginTime) {
+		this.beginTime = beginTime;
+	}
+
+	public String getBeginTime() {
+		return this.beginTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getEndTime() {
+		return this.endTime;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getCategoryName() {
+		return this.categoryName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public void setAddUserName(String addUserName) {
+		this.addUserName = addUserName;
+	}
+
+	public String getAddUserName() {
+		return this.addUserName;
+	}
+
+	public void setModifyUserName(String modifyUserName) {
+		this.modifyUserName = modifyUserName;
+	}
+
+	public String getModifyUserName() {
+		return this.modifyUserName;
+	}
+
+	public void setDisplaying(String displaying) {
+		this.displaying = displaying;
+	}
+
+	public String getDisplaying() {
+		return this.displaying;
+	}
+
+	public void setTnewsAudits(List<TnewsAudits> tnewsAudits) {
+		this.tnewsAudits = tnewsAudits;
+	}
+
+	public List<TnewsAudits> getTnewsAudits() {
+		return this.tnewsAudits;
+	}
+
+	public void setAconfigs(String aconfigs) {
+		this.aconfigs = aconfigs;
+	}
+
+	public String getAconfigs() {
+		return this.aconfigs;
+	}
+}
---
+++ jzfp/src/com/demo/jzfp/entity/ToFiles.java
@@ -0,0 +1,183 @@
+package com.demo.jzfp.entity;
+
+public class ToFiles {
+	private String contextPath;
+
+	private String page;
+
+	private String sortname;
+
+	private String sortorder;
+
+	private String infoYear;
+
+	private String addressXz;
+
+	private String addressC;
+
+	private String countrymanId;
+
+	private String countryman;
+
+	private String telphone;
+
+	private String familyMemers;
+
+	private String jdbfzrr;
+
+	private String bfzrrPost;
+
+	private String bfzrrPhone;
+
+	private String actionDl;
+
+	private String jtstRjsr;
+
+	private String jtstRjsr1;
+
+	private String aconfigs;
+
+	public void setContextPath(String contextPath) {
+		this.contextPath = contextPath;
+	}
+
+	public String getContextPath() {
+		return this.contextPath;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public String getPage() {
+		return this.page;
+	}
+
+	public void setSortname(String sortname) {
+		this.sortname = sortname;
+	}
+
+	public String getSortname() {
+		return this.sortname;
+	}
+
+	public void setSortorder(String sortorder) {
+		this.sortorder = sortorder;
+	}
+
+	public String getSortorder() {
+		return this.sortorder;
+	}
+
+	public void setInfoYear(String infoYear) {
+		this.infoYear = infoYear;
+	}
+
+	public String getInfoYear() {
+		return this.infoYear;
+	}
+
+	public void setAddressXz(String addressXz) {
+		this.addressXz = addressXz;
+	}
+
+	public String getAddressXz() {
+		return this.addressXz;
+	}
+
+	public void setAddressC(String addressC) {
+		this.addressC = addressC;
+	}
+
+	public String getAddressC() {
+		return this.addressC;
+	}
+
+	public void setCountrymanId(String countrymanId) {
+		this.countrymanId = countrymanId;
+	}
+
+	public String getCountrymanId() {
+		return this.countrymanId;
+	}
+
+	public void setCountryman(String countryman) {
+		this.countryman = countryman;
+	}
+
+	public String getCountryman() {
+		return this.countryman;
+	}
+
+	public void setTelphone(String telphone) {
+		this.telphone = telphone;
+	}
+
+	public String getTelphone() {
+		return this.telphone;
+	}
+
+	public void setFamilyMemers(String familyMemers) {
+		this.familyMemers = familyMemers;
+	}
+
+	public String getFamilyMemers() {
+		return this.familyMemers;
+	}
+
+	public void setJdbfzrr(String jdbfzrr) {
+		this.jdbfzrr = jdbfzrr;
+	}
+
+	public String getJdbfzrr() {
+		return this.jdbfzrr;
+	}
+
+	public void setBfzrrPost(String bfzrrPost) {
+		this.bfzrrPost = bfzrrPost;
+	}
+
+	public String getBfzrrPost() {
+		return this.bfzrrPost;
+	}
+
+	public void setBfzrrPhone(String bfzrrPhone) {
+		this.bfzrrPhone = bfzrrPhone;
+	}
+
+	public String getBfzrrPhone() {
+		return this.bfzrrPhone;
+	}
+
+	public void setActionDl(String actionDl) {
+		this.actionDl = actionDl;
+	}
+
+	public String getActionDl() {
+		return this.actionDl;
+	}
+
+	public void setJtstRjsr(String jtstRjsr) {
+		this.jtstRjsr = jtstRjsr;
+	}
+
+	public String getJtstRjsr() {
+		return this.jtstRjsr;
+	}
+
+	public void setJtstRjsr1(String jtstRjsr1) {
+		this.jtstRjsr1 = jtstRjsr1;
+	}
+
+	public String getJtstRjsr1() {
+		return this.jtstRjsr1;
+	}
+
+	public void setAconfigs(String aconfigs) {
+		this.aconfigs = aconfigs;
+	}
+
+	public String getAconfigs() {
+		return this.aconfigs;
+	}
+}
