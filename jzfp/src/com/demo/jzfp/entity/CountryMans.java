257e5e3b9cb133cb3a810a90a8b3b15dec8b38ba
Administrator <Administrator@PC-20151113BSSS>
2016-09-14 18:02:18 +0800

    jjy

---
+++ jzfp/src/com/demo/jzfp/entity/CountryMans.java
@@ -0,0 +1,475 @@
+package com.demo.jzfp.entity;
+
+import java.util.List;
+
+public class CountryMans {
+	private String contextPath;
+
+	private String page;
+
+	private String sortname;
+
+	private String sortorder;
+
+	private String pkhimg;
+
+	private String beginTime;
+
+	private String endTime;
+
+	private String address;
+
+	private String countryName;
+
+	private String tdataFamilys;
+
+	private String jkzk;
+
+	private String tdataActions;
+
+	private String tdataResult;
+
+	private String tdataHelper;
+
+	private String orgType;
+
+	private String countrymanId;
+
+	private String infoYear;
+
+	private String card;
+
+	private String countryId;
+
+	private String addressSz;
+
+	private String addressX;
+
+	private String addressXz;
+
+	private String addressC;
+
+	private String countryzb;
+
+	private String countryman;
+
+	private String poorState;
+
+	private String poorCard;
+
+	private String poorReason;
+
+	private String jhtpDate;
+
+	private String sex;
+
+	private String familyNo;
+
+	private int familyMemers;
+
+	private String telphone;
+
+	private String creator;
+
+	private String createTime;
+
+	private String remark;
+
+	private String whcd;
+
+	private int age;
+
+	private String waterArea;
+
+	private String gdArea;
+
+	private String slArea;
+
+	private String zfjg;
+
+	private String zzArea;
+
+	private String rjsrqk;
+
+	private List<TdataResult> tdataResults;
+
+	private List<TdataHelper> tdataHelpers;
+
+	private String aconfigs;
+
+	public void setContextPath(String contextPath) {
+		this.contextPath = contextPath;
+	}
+
+	public String getContextPath() {
+		return this.contextPath;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public String getPage() {
+		return this.page;
+	}
+
+	public void setSortname(String sortname) {
+		this.sortname = sortname;
+	}
+
+	public String getSortname() {
+		return this.sortname;
+	}
+
+	public void setSortorder(String sortorder) {
+		this.sortorder = sortorder;
+	}
+
+	public String getSortorder() {
+		return this.sortorder;
+	}
+
+	public void setPkhimg(String pkhimg) {
+		this.pkhimg = pkhimg;
+	}
+
+	public String getPkhimg() {
+		return this.pkhimg;
+	}
+
+	public void setBeginTime(String beginTime) {
+		this.beginTime = beginTime;
+	}
+
+	public String getBeginTime() {
+		return this.beginTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getEndTime() {
+		return this.endTime;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getCountryName() {
+		return this.countryName;
+	}
+
+	public void setTdataFamilys(String tdataFamilys) {
+		this.tdataFamilys = tdataFamilys;
+	}
+
+	public String getTdataFamilys() {
+		return this.tdataFamilys;
+	}
+
+	public void setJkzk(String jkzk) {
+		this.jkzk = jkzk;
+	}
+
+	public String getJkzk() {
+		return this.jkzk;
+	}
+
+	public void setTdataActions(String tdataActions) {
+		this.tdataActions = tdataActions;
+	}
+
+	public String getTdataActions() {
+		return this.tdataActions;
+	}
+
+	public void setTdataResult(String tdataResult) {
+		this.tdataResult = tdataResult;
+	}
+
+	public String getTdataResult() {
+		return this.tdataResult;
+	}
+
+	public void setTdataHelper(String tdataHelper) {
+		this.tdataHelper = tdataHelper;
+	}
+
+	public String getTdataHelper() {
+		return this.tdataHelper;
+	}
+
+	public void setOrgType(String orgType) {
+		this.orgType = orgType;
+	}
+
+	public String getOrgType() {
+		return this.orgType;
+	}
+
+	public void setCountrymanId(String countrymanId) {
+		this.countrymanId = countrymanId;
+	}
+
+	public String getCountrymanId() {
+		return this.countrymanId;
+	}
+
+	public void setInfoYear(String infoYear) {
+		this.infoYear = infoYear;
+	}
+
+	public String getInfoYear() {
+		return this.infoYear;
+	}
+
+	public void setCard(String card) {
+		this.card = card;
+	}
+
+	public String getCard() {
+		return this.card;
+	}
+
+	public void setCountryId(String countryId) {
+		this.countryId = countryId;
+	}
+
+	public String getCountryId() {
+		return this.countryId;
+	}
+
+	public void setAddressSz(String addressSz) {
+		this.addressSz = addressSz;
+	}
+
+	public String getAddressSz() {
+		return this.addressSz;
+	}
+
+	public void setAddressX(String addressX) {
+		this.addressX = addressX;
+	}
+
+	public String getAddressX() {
+		return this.addressX;
+	}
+
+	public void setAddressXz(String addressXz) {
+		this.addressXz = addressXz;
+	}
+
+	public String getAddressXz() {
+		return this.addressXz;
+	}
+
+	public void setAddressC(String addressC) {
+		this.addressC = addressC;
+	}
+
+	public String getAddressC() {
+		return this.addressC;
+	}
+
+	public void setCountryzb(String countryzb) {
+		this.countryzb = countryzb;
+	}
+
+	public String getCountryzb() {
+		return this.countryzb;
+	}
+
+	public void setCountryman(String countryman) {
+		this.countryman = countryman;
+	}
+
+	public String getCountryman() {
+		return this.countryman;
+	}
+
+	public void setPoorState(String poorState) {
+		this.poorState = poorState;
+	}
+
+	public String getPoorState() {
+		return this.poorState;
+	}
+
+	public void setPoorCard(String poorCard) {
+		this.poorCard = poorCard;
+	}
+
+	public String getPoorCard() {
+		return this.poorCard;
+	}
+
+	public void setPoorReason(String poorReason) {
+		this.poorReason = poorReason;
+	}
+
+	public String getPoorReason() {
+		return this.poorReason;
+	}
+
+	public void setJhtpDate(String jhtpDate) {
+		this.jhtpDate = jhtpDate;
+	}
+
+	public String getJhtpDate() {
+		return this.jhtpDate;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getSex() {
+		return this.sex;
+	}
+
+	public void setFamilyNo(String familyNo) {
+		this.familyNo = familyNo;
+	}
+
+	public String getFamilyNo() {
+		return this.familyNo;
+	}
+
+	public void setFamilyMemers(int familyMemers) {
+		this.familyMemers = familyMemers;
+	}
+
+	public int getFamilyMemers() {
+		return this.familyMemers;
+	}
+
+	public void setTelphone(String telphone) {
+		this.telphone = telphone;
+	}
+
+	public String getTelphone() {
+		return this.telphone;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public String getCreator() {
+		return this.creator;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getCreateTime() {
+		return this.createTime;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getRemark() {
+		return this.remark;
+	}
+
+	public void setWhcd(String whcd) {
+		this.whcd = whcd;
+	}
+
+	public String getWhcd() {
+		return this.whcd;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setWaterArea(String waterArea) {
+		this.waterArea = waterArea;
+	}
+
+	public String getWaterArea() {
+		return this.waterArea;
+	}
+
+	public void setGdArea(String gdArea) {
+		this.gdArea = gdArea;
+	}
+
+	public String getGdArea() {
+		return this.gdArea;
+	}
+
+	public void setSlArea(String slArea) {
+		this.slArea = slArea;
+	}
+
+	public String getSlArea() {
+		return this.slArea;
+	}
+
+	public void setZfjg(String zfjg) {
+		this.zfjg = zfjg;
+	}
+
+	public String getZfjg() {
+		return this.zfjg;
+	}
+
+	public void setZzArea(String zzArea) {
+		this.zzArea = zzArea;
+	}
+
+	public String getZzArea() {
+		return this.zzArea;
+	}
+
+	public void setRjsrqk(String rjsrqk) {
+		this.rjsrqk = rjsrqk;
+	}
+
+	public String getRjsrqk() {
+		return this.rjsrqk;
+	}
+
+	public void setTdataResults(List<TdataResult> tdataResults) {
+		this.tdataResults = tdataResults;
+	}
+
+	public List<TdataResult> getTdataResults() {
+		return this.tdataResults;
+	}
+
+	public void setTdataHelpers(List<TdataHelper> tdataHelpers) {
+		this.tdataHelpers = tdataHelpers;
+	}
+
+	public List<TdataHelper> getTdataHelpers() {
+		return this.tdataHelpers;
+	}
+
+	public void setAconfigs(String aconfigs) {
+		this.aconfigs = aconfigs;
+	}
+
+	public String getAconfigs() {
+		return this.aconfigs;
+	}
+}
